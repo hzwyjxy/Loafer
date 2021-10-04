aea584005b7e3e001adf1639618dcd3f46eba9d8
maverick <980792732@qq.com>
2021-10-04 15:37:29 +0800

    pre mem

--- src/main/java/loafer4vm/StartLoafer.java
+++ src/main/java/loafer4vm/StartLoafer.java
@@ -6,7 +6,7 @@ public class StartLoafer {
         System.out.println("Loading...");
         String mode = System.getProperty("mode");
         String core=System.getProperty("core");
-        String memory=System.getProperty("memory"); //useless,because we change the Xmx first
+        String memory=System.getProperty("memory"); //useful
         String storage=System.getProperty("storage");
         String network=System.getProperty("network");
         String gpu=null; //gpu should be used to play games!!!!!
